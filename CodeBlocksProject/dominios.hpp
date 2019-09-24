32fd7e1a19a3d9b8ce468ab4cc10ea7c47d00b56
RonaldCDO <ronaldcesar.eng@gmail.com>
2019-09-24 10:00:24 -0300

    Padronização de Codificação

--- CodeBlocksProject/dominios.hpp
+++ CodeBlocksProject/dominios.hpp
@@ -1,3 +1,11 @@
+/* Autores: Matheus ..., Ronald Oliveira
+ * Matrículas: ..., 180047205
+ * Universidade de Brasilia, 2º Semestre de 2019
+ * Disciplina: Técnicas de Programação 1
+ * Professor: Fernando Albuquerque
+ * Linguagem utilizada: C++
+ */
+
 #pragma once
 
 #include <iostream>
@@ -6,15 +14,17 @@
 
 using namespace std;
 
-//Declaracao de Classes.
+ // Declaração dos Domínios
 
-class Dominio {
+class Dominio
+{
     private:
         string valor;
         virtual void validar(string) = 0;
     public:
         void setValor(string);
-        string getValor(){
+        string getValor()
+        {
             return valor;
         }
         static bool CaracterLetra(char);
@@ -22,106 +32,158 @@ class Dominio {
         static bool AlgoritmoDeLuhn(string);
 };
 
-//Ultilizacao de heranca para o atributo valor de Dominios e os metodos, set, get e validar.
-class Assento : public Dominio{
+
+
+class Assento : public Dominio
+{
     private:
         void validar(string);
 };
 
-class Bagagem : public Dominio{
+
+
+class Bagagem : public Dominio
+{
     private:
-        //Declarando o Tamanho do "vetor" string.
         const static int TAMANHO = 1;
         void validar(string);
 };
 
-class CodigoDeBanco : public Dominio{
+
+
+class CodigoDeBanco : public Dominio
+{
     private:
         const static int TAMANHO = 3;
         void validar(string);
 };
 
-class CodigoDeCarona : public Dominio{
+
+
+class CodigoDeCarona : public Dominio
+{
     private:
         const static int TAMANHO = 4;
         void validar(string);
 };
 
-class CodigoDeReserva : public Dominio{
+
+
+class CodigoDeReserva : public Dominio
+{
     private:
         const static int TAMANHO = 5;
         void validar(string);
 };
 
-class Cidade : public Dominio{
+
+
+class Cidade : public Dominio
+{
     private:
         const static int TAMANHO = 10;
         void validar(string);
 };
 
-class CPF : public Dominio{
+
+
+class CPF : public Dominio
+{
     private:
         const static int TAMANHO = 14;
         void validar(string);
 };
 
-class Data : public Dominio{
+
+
+class Data : public Dominio
+{
     private:
         const static int TAMANHO = 10;
         void validar(string);
 };
 
-class Duracao : public Dominio{
+
+
+class Duracao : public Dominio
+{
     private:
         void validar(string);
 };
 
-class Estado : public Dominio{
+
+
+class Estado : public Dominio
+{
     private:
         const static int TAMANHO = 2;
         void validar(string);
 };
 
-class Email : public Dominio{
+
+
+class Email : public Dominio
+{
     private:
         void validar(string);
 };
 
-class Nome : public Dominio{
+
+
+class Nome : public Dominio
+{
     private:
         void validar(string);
 };
 
-class NumeroDeAgencia: public Dominio{
+
+
+class NumeroDeAgencia: public Dominio
+{
     private:
         const static int TAMANHO = 6;
         void validar(string);
 };
 
-class NumeroDeConta : public Dominio{
+
+
+class NumeroDeConta : public Dominio
+{
     private:
         const static int TAMANHO = 8;
         void validar(string);
 };
 
-class Preco : public Dominio{
+
+
+class Preco : public Dominio
+{
     private:
         void validar(string);
 };
 
-class Telefone : public Dominio{
+
+
+class Telefone : public Dominio
+{
     private:
         const static int TAMANHO = 15;
         void validar(string);
 };
 
-class Senha : public Dominio{
+
+
+class Senha : public Dominio
+{
     private:
         const static int TAMANHO = 5;
         void validar(string);
 };
 
-class Vagas : public Dominio{
+
+
+class Vagas : public Dominio
+{
     private:
         const static int TAMANHO = 1;
         void validar(string);
